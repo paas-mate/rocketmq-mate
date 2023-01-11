559ba62cc394709c653e3fa48f24afdd53554c8e
Lico-Tom <95178212+Lico-Tom@users.noreply.github.com>
2023-01-11 16:31:59 +0800

    Based on the ClUSTER_ROLE environment variable, start rocketmq. (#1)

--- docker-build/scripts/start-daemon.sh
+++ docker-build/scripts/start-daemon.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 
 mkdir -p $ROCKETMQ_HOME/logs
+
+if [ "$CLUSTER_ROLE" == "NAMESRV" ]; then
+  ${$ROCKETMQ_HOME}/bin/mqnamesrv &
+elif [ "$CLUSTER_ROLE" == "MQBROKER" ]; then
+  ${ROCKETMQ_HOME}/bin/mqbroker -n ${BROKER_ADDR:="localhost:9876"} &
+else
+  ${ROCKETMQ_HOME}/bin/mqnamesrv &
+  sleep 15s
+  ${ROCKETMQ_HOME}/bin/mqbroker/mqbroker -n ${BROKER_ADDR:="localhost:9876"} &
+fi
