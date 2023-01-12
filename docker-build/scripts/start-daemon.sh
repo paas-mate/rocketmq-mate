a740ed7a233cf46540eba581334b980d0091ae76
goflutterjava <goflutterjava@gmail.com>
2023-01-12 17:25:20 +0800

    optimize script

--- docker-build/scripts/start-daemon.sh
+++ docker-build/scripts/start-daemon.sh
@@ -2,12 +2,21 @@
 
 mkdir -p $ROCKETMQ_HOME/logs
 
+sed -i 's#\${user.home}/logs#/opt/rocketmq/logs#g' $ROCKETMQ_HOME/conf/logback*.xml
+
+NAMESRV_ADDR=${ROCKETMQ_NAMESRV_ADDR}
+
+if [ -z "${REDIS_HOST}" ]
+then
+    NAMESRV_ADDR="localhost:9876"
+fi
+
 if [ "$CLUSTER_ROLE" == "NAMESRV" ]; then
   ${$ROCKETMQ_HOME}/bin/mqnamesrv &
 elif [ "$CLUSTER_ROLE" == "MQBROKER" ]; then
-  ${ROCKETMQ_HOME}/bin/mqbroker -n ${BROKER_ADDR:="localhost:9876"} &
+  ${ROCKETMQ_HOME}/bin/mqbroker -n $NAMESRV_ADDR &
 else
   ${ROCKETMQ_HOME}/bin/mqnamesrv &
   sleep 15s
-  ${ROCKETMQ_HOME}/bin/mqbroker/mqbroker -n ${BROKER_ADDR:="localhost:9876"} &
+  ${ROCKETMQ_HOME}/bin/mqbroker -n $NAMESRV_ADDR &
 fi
