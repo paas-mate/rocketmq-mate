4cc1b1f4d48877e128e8b993fb148018191d5d4b
xiang hu <xiang092689@qq.com>
2023-01-27 22:37:13 +0800

    support NAMESRV_INTRERFACE

--- docker-build/scripts/start-daemon.sh
+++ docker-build/scripts/start-daemon.sh
@@ -9,6 +9,8 @@ NAMESRV_ADDR=${ROCKETMQ_NAMESRV_ADDR}
 if [ -z "${REDIS_HOST}" ]
 then
     NAMESRV_ADDR="localhost:9876"
+elif [ -z "${NAMESRV_INTRERFACE}" ]; then
+    NAMESRV_ADDR=$(ip addr show ${NAMESRV_INTRERFACE} | grep inet | grep -v inet6 | grep '/24' | awk '{print $2}' | awk -F '/' '{print $1}')
 fi
 
 if [ "$CLUSTER_ROLE" == "NAMESRV" ]; then
